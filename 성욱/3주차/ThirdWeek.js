e25784d15f8597b8827f78eace161f1b9c64f670
kso2749 <rokmctkd67@naver.com>
2021-07-26 16:33:05 +0900

    .

--- 성욱/3주차/ThirdWeek.js
+++ 성욱/3주차/ThirdWeek.js
@@ -60,3 +60,86 @@ ksw.age = 24;
 console.log(ksw);
 console.log(person4);
 
+
+
+//===================================================================================================
+// #8 / Array
+//===================================================================================================
+
+
+// 2. Index position
+
+const number =[1,2,3];
+
+console.log(number); // 배열 전체 출력
+console.log(number.length); // 배열 길이 출력
+console.log(number[1]); //배열의 해당 인덱스 값 출력
+
+// 3. Looping over an array (배열의 모든 값 출력)
+
+// a. for
+console.log("for");
+
+for(let i=0; i<number.length; i++){
+    console.log(number[i]);
+}
+
+// b. for of
+
+console.log("for of");
+for(let num of number){
+    console.log(num);
+}
+
+// c. forEach (콜백함수)
+console.log("forEach");
+number.forEach((number) => console.log(number));
+
+
+// 4. 삽입, 삭제
+
+// push : 배열 끝에 값 추가
+number.push(4);
+console.log(number);
+
+// pop : 배열 끝 값 제거
+number.pop();
+console.log(number);
+
+
+// shift, unshift는 push, pop에 비해 연산 속도 느림
+// unshift : 배열 처음에 값 추가
+number.unshift(0);
+console.log(number);
+
+// shift : 배열 첫번째 값 제거
+number.shift();
+console.log(number);
+
+// splice(startIndex, deleteCount) : 원하는 index 값 삭제, 어디서부터 몇개 지울건지
+
+number.push(4,5,6);
+console.log(number);
+
+number.splice(3, 1);
+console.log(number);
+
+number.splice(3, 2, 4); // 3번부터 두개를 지우고 4 추가
+console.log(number);
+
+ // 두가지 배열 묶기
+
+ const number2 = [5,6,7];
+
+ const allNumber = number.concat(number2);
+ console.log(allNumber);
+
+
+ // 5. 검색
+ // find the index
+
+ console.log(number); // 1 2 3 4
+ console.log(number.indexOf('가')); // 2
+ console.log(number.includes(6)); // false
+ number.push(1); // number : [ 1 2 3 4 1 ]
+ console.log(number.lastIndexOf(1)); // 4
